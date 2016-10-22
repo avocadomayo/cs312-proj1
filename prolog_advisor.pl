7420061dc41a6a156a733d404f62f083a96ef5e6
avocadomayo <wendy_jy_liu@hotmail.com>
2016-10-22 12:36:53 -0700

    completed database of all undergrad cs courses

--- prolog_advisor.pl
+++ prolog_advisor.pl
@@ -516,6 +516,132 @@ prop(cs344_101, credits, 3).
 prop(cs344_101, activity, lecture).
 prop(cs344_101, instructor, maclean).
 
+prop(cs404_101, code, cpsc404).
+prop(cs404_101, year, 4).
+prop(cs404_101, section, 101).
+prop(cs404_101, title, [advanced, relational, databases]).
+prop(cs404_101, term, 1).
+prop(cs404_101, credits, 3).
+prop(cs404_101, activity, lecture).
+prop(cs404_101, instructor, knorr).
+
+prop(cs406_101, code, cpsc406).
+prop(cs404_101, year, 4).
+prop(cs404_101, section, 101).
+prop(cs404_101, title, [computational, optimization]).
+prop(cs404_101, term, 1).
+prop(cs404_101, credits, 3).
+prop(cs404_101, activity, lecture).
+prop(cs404_101, instructor, ascher).
+
+prop(cs411_201, code, cpsc411).
+prop(cs411_201, year, 4).
+prop(cs411_201, section, 201).
+prop(cs411_201, title, [introduction, to, compiler, construction]).
+prop(cs411_201, term, 2).
+prop(cs411_201, credits, 3).
+prop(cs411_201, activity, lecture).
+prop(cs411_201, instructor, garcia).
+
+prop(cs415_101, code, cpsc415).
+prop(cs415_101, year, 4).
+prop(cs415_101, section, 101).
+prop(cs415_101, title, [advanced, operating, systems]).
+prop(cs415_101, term, 1).
+prop(cs415_101, credits, 3).
+prop(cs415_101, activity, lecture).
+prop(cs415_101, instructor, acton).
+
+prop(cs416_201, code, cpsc416).
+prop(cs416_201, year, 4).
+prop(cs416_201, section, 201).
+prop(cs416_201, title, [distributed, systems]).
+prop(cs416_201, term, 2).
+prop(cs416_201, credits, 3).
+prop(cs416_201, activity, lecture).
+prop(cs416_201, instructor, beschastnikh).
+
+prop(cs418_201, code, cpsc418).
+prop(cs418_201, year, 4).
+prop(cs418_201, section, 201).
+prop(cs418_201, title, [parallel, computation]).
+prop(cs418_201, term, 2).
+prop(cs418_201, credits, 3).
+prop(cs418_201, activity, lecture).
+prop(cs418_201, instructor, greenstreet).
+
+prop(cs420_201, code, cpsc420).
+prop(cs420_201, year, 4).
+prop(cs420_201, section, 201).
+prop(cs420_201, title, [advanced, algorithms, design, and, analysis]).
+prop(cs420_201, term, 2).
+prop(cs420_201, credits, 3).
+prop(cs420_201, activity, lecture).
+prop(cs420_201, instructor, evans).
+
+prop(cs421_101, code, cpsc421).
+prop(cs421_101, year, 4).
+prop(cs421_101, section, 101).
+prop(cs421_101, title, [introduction, to, theory, of, computing]).
+prop(cs421_101, term, 1).
+prop(cs421_101, credits, 3).
+prop(cs421_101, activity, lecture).
+prop(cs421_101, instructor, harvey).
+
+prop(cs422_101, code, cpsc422).
+prop(cs422_101, year, 4).
+prop(cs422_101, section, 101).
+prop(cs422_101, title, [intelligent, systems]).
+prop(cs422_101, term, 1).
+prop(cs422_101, credits, 3).
+prop(cs422_101, activity, lecture).
+prop(cs422_101, instructor, carenini).
+
+prop(cs425_201, code, cpsc425).
+prop(cs425_201, year, 4).
+prop(cs425_201, section, 201).
+prop(cs425_201, title, [computer, vision]).
+prop(cs425_201, term, 2).
+prop(cs425_201, credits, 3).
+prop(cs425_201, activity, lecture).
+prop(cs425_201, instructor, little).
+
+prop(cs426_201, code, cpsc426).
+prop(cs426_201, year, 4).
+prop(cs426_201, section, 201).
+prop(cs426_201, title, [computer, animation]).
+prop(cs426_201, term, 2).
+prop(cs426_201, credits, 3).
+prop(cs426_201, activity, lecture).
+prop(cs426_201, instructor, vandepanne).
+
+prop(cs430_101, code, cpsc430).
+prop(cs430_101, year, 4).
+prop(cs430_101, section, 101).
+prop(cs430_101, title, [computers, and, society]).
+prop(cs430_101, term, 1).
+prop(cs430_101, credits, 3).
+prop(cs430_101, activity, lecture).
+prop(cs430_101, instructor, dawson).
+
+prop(cs444_201, code, cpsc444).
+prop(cs444_201, year, 4).
+prop(cs444_201, section, 201).
+prop(cs444_201, title, [advanced, methods, for, human, computer, interaction]).
+prop(cs444_201, term, 2).
+prop(cs444_201, credits, 3).
+prop(cs444_201, activity, lecture).
+prop(cs444_201, instructor, mcgrenere).
+
+prop(cs445_201, code, cpsc445).
+prop(cs445_201, year, 4).
+prop(cs445_201, section, 201).
+prop(cs445_201, title, [algorithms, in, bioinformatics]).
+prop(cs445_201, term, 2).
+prop(cs445_201, credits, 3).
+prop(cs445_201, activity, lecture).
+prop(cs445_201, instructor, hoos).
+
 /* Try the following queries
 | ?- ask([who, is, teaching, cpsc100], A).
 | ?- ask([who, is, an, instructor, that, teaches, cpsc103], A).
